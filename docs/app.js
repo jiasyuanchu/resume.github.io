b447152e4ff5944ee0da4efd8744fb92b76a9b09
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-08-16 11:26:26 +0800

    feat: add docs

---
+++ docs/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
+
+module.exports = app
